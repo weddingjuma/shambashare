7c392a31c0792186d77105ea8fc7aa37a7e97a66
Denzel Wamburu <denzjoseph@gmail.com>
2017-10-20 20:22:01 +0300

    Use arrow function to enable get user email

--- src/pages/newshamba/newshamba.ts
+++ src/pages/newshamba/newshamba.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  LoadingController
+} from "ionic-angular";
 import firebase from "firebase";
-import { AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
-import {ShambalistPage } from '../shambalist/shambalist';
+import {
+  AngularFireDatabase,
+  FirebaseListObservable
+} from "angularfire2/database";
+import { ShambalistPage } from "../shambalist/shambalist";
 /**
  * Generated class for the NewshambaPage page.
  *
@@ -11,53 +19,69 @@ import {ShambalistPage } from '../shambalist/shambalist';
  */
 @IonicPage()
 @Component({
-  selector: 'page-newshamba',
-  templateUrl: 'newshamba.html',
+  selector: "page-newshamba",
+  templateUrl: "newshamba.html"
 })
 export class NewshambaPage {
   newShamba = {
-    location: '',
-    size: '',
-    price: '',
-    user: ''
+    location: "",
+    size: "",
+    price: "",
+    user: ""
   };
+  useremail: any;
   shambas: FirebaseListObservable<any[]>;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public db: AngularFireDatabase,
+    public loadingCtrl: LoadingController
+  ) {
+    this.shambas = db.list("/shambas");
+    console.log(this.shambas);
+    var user = firebase.auth().currentUser;
+    
+    if (user != null) {
+      // user.providerData.forEach(function (profile) {
+      //   console.log("Sign-in provider: "+profile.providerId);
+      //   console.log("  Provider-specific UID: "+profile.uid);
+      //   console.log("  Name: "+profile.displayName);
+      //   console.log("  Email: "+profile.email);
+      //   console.log("  Photo URL: "+profile.photoURL);
+
+      //   this.newShamba.user = profile.email;
+      // });
+      user.providerData.forEach(profile =>{
+        console.log("Sign-in provider: "+profile.providerId);
+          console.log("  Provider-specific UID: "+profile.uid);
+          console.log("  Name: "+profile.displayName);
+          console.log("  Email: "+profile.email);
+          console.log("  Photo URL: "+profile.photoURL);
   
-  constructor( public navCtrl: NavController,
-               public navParams: NavParams,
-               public db: AngularFireDatabase,
-               public loadingCtrl: LoadingController
-              ) {
-                this.shambas = db.list('/shambas');
-                console.log(this.shambas)
+          this.newShamba.user = profile.email;
+      });
+    }
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad NewshambaPage');
+    console.log("ionViewDidLoad NewshambaPage");
   }
 
   addShamba(newShamba) {
     let loader = this.loadingCtrl.create({
       content: "Posting Shamba ... "
     });
-    firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
-        // User is signed in.
-        console.log("User posting:", user)
-      this.newshamba.user = user;
-      } else {
-        // No user is signed in.
-      }
-    });
+    // this.newShamba.user = this.useremail;
     loader.present();
-   this.shambas.push(this.newShamba);
-   this.newShamba = {
-     location: '',
-     size: '',
-     price: '',
-     user: ''
-   }
-   loader.dismiss();
-   this.navCtrl.push(ShambalistPage);
+    this.shambas.push(this.newShamba);
+    this.newShamba = {
+      location: "",
+      size: "",
+      price: "",
+      user: ""
+    };
+    loader.dismiss();
+    this.navCtrl.push(ShambalistPage);
   }
 }
